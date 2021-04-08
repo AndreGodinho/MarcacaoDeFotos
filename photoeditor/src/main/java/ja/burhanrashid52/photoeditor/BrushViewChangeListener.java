7fb6c31209c18c6629fae7579ff56d60eb7bbbef
AndreLocal <andre.godinho1971@gmail.com>
2021-04-08 09:44:11 -0400

    Sistema de Marcação de Fotos comprado na Workana

---
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/BrushViewChangeListener.java
@@ -0,0 +1,11 @@
+package ja.burhanrashid52.photoeditor;
+
+interface BrushViewChangeListener {
+    void onViewAdd(BrushDrawingView brushDrawingView);
+
+    void onViewRemoved(BrushDrawingView brushDrawingView);
+
+    void onStartDrawing();
+
+    void onStopDrawing();
+}
